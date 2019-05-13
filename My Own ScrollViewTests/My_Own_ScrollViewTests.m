f2b1d121a8951cab5415c4d2419bf835cae2fe90
LuizRamos <luizramospe@hotmail.com>
2019-05-13 11:03:23 -0400

    My Own ScrollView

---
+++ My Own ScrollViewTests/My_Own_ScrollViewTests.m
@@ -0,0 +1,37 @@
+//
+//  My_Own_ScrollViewTests.m
+//  My Own ScrollViewTests
+//
+//  Created by Luiz on 5/13/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface My_Own_ScrollViewTests : XCTestCase
+
+@end
+
+@implementation My_Own_ScrollViewTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
